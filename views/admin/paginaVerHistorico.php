1b93ff29295a0fc5c2343b104ad28e37a6444b3c
OscarMVieira <oscar.vieira@ipcbcampus.pt>
2026-03-30 17:01:40 +0100

    Fix-(SCRUM24)- Update/Fix da centralização

--- views/admin/paginaVerHistorico.php
+++ views/admin/paginaVerHistorico.php
@@ -1,49 +1,44 @@
 <?php include '../templates/header.php'; ?>
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <title>Histórico: João</title>
-    <link rel="stylesheet" href="../../public/css/paginaHistorico.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
-    <div class="layoutPrincipal">
-        <main class="areaConteudo">
-            <h1 class="tituloEsquerda">Histórico: João</h1>
 
-            <div class="caixaCard containerTabela">
-                <table class="tabelaHistorico">
-                    <thead>
-                        <tr>
-                            <th>Livro</th>
-                            <th>Data Requisição</th>
-                            <th>Data entrega</th>
-                            <th>Estado</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>O Alquimista</td>
-                            <td>28/03/2026</td>
-                            <td>11/04/2026</td>
-                            <td>Entregue</td>
-                        </tr>
-                        <tr>
-                            <td>Dom Quixote</td>
-                            <td>28/03/2026</td>
-                            <td>11/04/2026</td>
-                            <td>Entregue</td>
-                        </tr>
-                        </tbody>
-                </table>
-            </div>
+<link rel="stylesheet" href="../../public/css/paginaHistorico.css">
 
-            <div class="alinhamentoVoltar">
-                <a href="../cliente/paginaCatalogo.php" class="botaoBranco">Voltar</a>
-            </div>
-        </main>
+<section class="seccaoHistorico">
+    <div class="containerHistorico">
+        
+        <h1 class="tituloHistorico">Histórico: João</h1>
+
+        <div class="cartaoTabela">
+            <table class="tabelaHistorico">
+                <thead>
+                    <tr>
+                        <th>Livro</th>
+                        <th>Data Requisição</th>
+                        <th>Data entrega</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>O Alquimista</td>
+                        <td>28/03/2026</td>
+                        <td>11/04/2026</td>
+                        <td>Entregue</td>
+                    </tr>
+                    <tr>
+                        <td>Dom Quixote</td>
+                        <td>28/03/2026</td>
+                        <td>11/04/2026</td>
+                        <td>Entregue</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="areaBotoes">
+            <a href="../cliente/paginaCatalogo.php" class="botaoVoltar">Voltar</a>
+        </div>
+
     </div>
-</body>
-</html>
+</section>
+
 <?php include '../templates/footer.php'; ?>
