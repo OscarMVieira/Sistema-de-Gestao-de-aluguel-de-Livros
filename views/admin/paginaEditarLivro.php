57e854ea78cf784fb0226ce01305e56efcbf2bb3
OscarMVieira <oscar.vieira@ipcbcampus.pt>
2026-04-13 17:23:18 +0100

    feature-(SCRUM58)-Feature processaEdicao e pagina livro atualizada

--- views/admin/paginaEditarLivro.php
+++ views/admin/paginaEditarLivro.php
@@ -1,6 +1,13 @@
 <?php 
 $activePage = 'catalogo'; 
 include '../templates/header.php'; 
+require_once '../basedados/basedados.h'; 
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM livros WHERE ID_Livro = $id";
+$resultado = $conn->query($sql);
+$livro = $resultado->fetch_assoc();
 ?>
 
 <link rel="stylesheet" href="../../public/css/detalhesLivro.css">
@@ -9,42 +16,47 @@ include '../templates/header.php';
 <div class="centralizador-pagina">
     <h1 class="tituloPagina">Editar Livro</h1>
 
-    <div class="gradeInfo">
-        <section class="caixaCard selecaoCapa">
-            <h2 class="tituloCard">Editar Capa</h2>
-            <p class="textoUpload">Upload Capa</p>
-            <div class="bordaImagem">
-                <img src="https://m.media-amazon.com/images/I/71jY8t-W3vL.jpg" alt="Capa">
-            </div>
-            <button class="botaoAzulPequeno">Mudar Capa</button>
-        </section>
-
-        <section class="caixaCard detalhesLivro">
-            <h2 class="tituloCardSublinhado">Informação Principal</h2>
-            <div class="linhaForm">
-                <input type="text" value="Nome do Livro">
-                <input type="text" value="Gênero do Livro">
-            </div>
-            <div class="linhaForm">
-                <input type="text" value="Autor do Livro">
-                <input type="text" value="ID do Livro">
-            </div>
-            <div class="containerCampos">
-                <div class="campoIndividual"><label>Data:</label><input type="text"></div>
-                <div class="campoIndividual"><label>Preço:</label><input type="text"></div>
-                <div class="campoIndividual"><label>Qtd:</label><input type="text"></div>
-            </div>
-        </section>
-    </div>
-
-    <div class="containerAcoesFinal">
-        <button class="botaoVoltar">Editar Livro</button>
+    <form action="processaEdicao.php" method="POST" enctype="multipart/form-data">
         
-        <div class="grupoBotoesBrancos">
-            <button class="botaoBranco">Limpar Formulário</button>
-            <a href="../cliente/paginaCatalogo.php" class="botaoBranco">Voltar</a>
+        <input type="hidden" name="id" value="<?php echo $livro['ID_Livro']; ?>">
+
+        <div class="gradeInfo">
+            <section class="caixaCard selecaoCapa">
+                <h2 class="tituloCard">Editar Capa</h2>
+                <p class="textoUpload">Upload Capa</p>
+                <div class="bordaImagem">
+                    <img src="../../public/img/<?php echo $livro['Capa']; ?>" alt="Capa">
+                </div>
+                <input type="file" name="capa" style="margin-top: 10px;">
+                <button type="button" class="botaoAzulPequeno">Mudar Capa</button>
+            </section>
+
+            <section class="caixaCard detalhesLivro">
+                <h2 class="tituloCardSublinhado">Informação Principal</h2>
+                <div class="linhaForm">
+                    <input type="text" name="titulo" value="<?php echo htmlspecialchars($livro['Titulo_Livro']); ?>">
+                    <input type="text" name="genero" value="<?php echo htmlspecialchars($livro['Genero']); ?>">
+                </div>
+                <div class="linhaForm">
+                    <input type="text" name="autor" value="<?php echo htmlspecialchars($livro['Autor_Livro']); ?>">
+                    <input type="text" value="<?php echo $livro['ID_Livro']; ?>" readonly>
+                </div>
+                <div class="containerCampos">
+                    <div class="campoIndividual"><label>Data:</label><input type="text" name="data" value="--"></div>
+                    <div class="campoIndividual"><label>Preço:</label><input type="text" name="preco" value="--"></div>
+                    <div class="campoIndividual"><label>Qtd:</label><input type="text" name="quantidade" value="<?php echo $livro['Quantidade']; ?>"></div>
+                </div>
+            </section>
         </div>
-    </div>
-</div>
+
+        <div class="containerAcoesFinal">
+            <button type="submit" class="botaoVoltar">Editar Livro</button>
+            
+            <div class="grupoBotoesBrancos">
+                <button type="reset" class="botaoBranco">Limpar Formulário</button>
+                <a href="paginaConsultarLivro.php?id=<?php echo $id; ?>" class="botaoBranco">Voltar</a>
+            </div>
+        </div>
+    </form> </div>
 
 <?php include '../templates/footer.php'; ?>
