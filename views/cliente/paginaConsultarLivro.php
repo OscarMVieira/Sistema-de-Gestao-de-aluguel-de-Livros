fac138232b00e9954dbe8bffaf2a7df444a9a617
OscarMVieira <oscar.vieira@ipcbcampus.pt>
2026-04-13 16:27:37 +0100

    feature-(SCRUM-58)-Página de detalhes do cliente já conecta à BD

--- views/cliente/paginaConsultarLivro.php
+++ views/cliente/paginaConsultarLivro.php
@@ -1,6 +1,29 @@
 <?php 
 $activePage = 'catalogo'; 
-include '../templates/headerCliente.php'; ?>
+include '../templates/headerCliente.php'; 
+require_once '../basedados/basedados.h'; // Adicionado para ligar à base de dados
+
+// 1. Verificar se o ID do livro foi passado no URL
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
+    // 2. Procurar os dados do livro específico
+    $sql = "SELECT * FROM livros WHERE ID_Livro = $id";
+    $resultado = $conn->query($sql);
+    
+    if ($resultado->num_rows > 0) {
+        $livro = $resultado->fetch_assoc();
+    } else {
+        echo "<div class='centralizador-pagina'><h2>Livro não encontrado.</h2></div>";
+        include '../templates/footer.php';
+        exit;
+    }
+} else {
+    echo "<div class='centralizador-pagina'><h2>Nenhum livro selecionado.</h2></div>";
+    include '../templates/footer.php';
+    exit;
+}
+?>
 
 <link rel="stylesheet" href="../../public/css/detalhesLivro.css">
 
@@ -11,24 +34,24 @@ include '../templates/headerCliente.php'; ?>
         <section class="caixaCard selecaoCapa">
             <h2 class="tituloCard">Capa</h2>
             <div class="bordaImagem">
-                <img src="https://m.media-amazon.com/images/I/71jY8t-W3vL.jpg" alt="Capa">
+                <img src="../../public/img/<?php echo $livro['Capa']; ?>" alt="Capa">
             </div>
         </section>
 
         <section class="caixaCard detalhesLivro">
             <h2 class="tituloCardSublinhado">Informação Principal</h2>
             <div class="linhaForm">
-                <input type="text" value="Nome do Livro">
-                <input type="text" value="Gênero do Livro">
+                <input type="text" value="<?php echo htmlspecialchars($livro['Titulo_Livro']); ?>" readonly>
+                <input type="text" value="<?php echo htmlspecialchars($livro['Genero']); ?>" readonly>
             </div>
             <div class="linhaForm">
-                <input type="text" value="Autor do Livro">
-                <input type="text" value="ID do Livro">
+                <input type="text" value="<?php echo htmlspecialchars($livro['Autor_Livro']); ?>" readonly>
+                <input type="text" value="<?php echo $livro['ID_Livro']; ?>" readonly>
             </div>
             <div class="containerCampos">
-                <div class="campoIndividual"><label>Data:</label><input type="text"></div>
-                <div class="campoIndividual"><label>Preço:</label><input type="text"></div>
-                <div class="campoIndividual"><label>Qtd:</label><input type="text"></div>
+                <div class="campoIndividual"><label>Data:</label><input type="text" value="--" readonly></div>
+                <div class="campoIndividual"><label>Preço:</label><input type="text" value="--" readonly></div>
+                <div class="campoIndividual"><label>Qtd:</label><input type="text" value="<?php echo $livro['Quantidade']; ?>" readonly></div>
             </div>
         </section>
     </div>
